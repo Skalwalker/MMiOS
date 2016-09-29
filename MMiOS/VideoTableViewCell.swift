81b9a12cc682ffba6e1a88fa7f279bb6ecc391f1
William <williamsimiao@gmail.com>
2016-09-28 23:54:18 -0300

    classes iniciais para Video e gitIgnore

---
+++ MMiOS/VideoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  VideoTableViewCell.swift
+//  MMiOS
+//
+//  Created by William on 28/09/16.
+//  Copyright © 2016 Roadway to SS. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
