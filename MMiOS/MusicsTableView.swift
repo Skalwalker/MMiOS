d45743810bfcf362d9cca1c5156d3a13226d04a3
Renato Nobre <rekanobre@gmail.com>
2016-09-30 14:41:27 -0300

    Music Assets

---
+++ MMiOS/MusicsTableView.swift
@@ -0,0 +1,13 @@
+//
+//  MusicsTableView.swift
+//  MMiOS
+//
+//  Created by Renato Nobre on 30/09/16.
+//  Copyright © 2016 Roadway to SS. All rights reserved.
+//
+
+import UIKit
+
+class MusicsTableView: NSObject {
+
+}
