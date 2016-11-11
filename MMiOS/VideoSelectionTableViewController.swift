cd1c2b66dee39e615199cf6023fda1ab9bbd58ec
William <williamsimiao@gmail.com>
2016-11-11 18:22:53 -0200

    coisas

--- MMiOS/VideoSelectionTableViewController.swift
+++ MMiOS/VideoSelectionTableViewController.swift
@@ -18,6 +18,43 @@ class VideoSelectionTableViewController: UIViewController, UITableViewDelegate, 
     @IBOutlet weak var videosLibrary: UILabel!
     var backgroundColor = ColorWeel()
     @IBOutlet weak var tableView: UITableView!
+    @IBAction func streamAction(_ sender: AnyObject) {
+        let alertController = UIAlertController(title: "Informe a URL da streaming", message: "", preferredStyle: .alert)
+        //cancel action
+        let cancelAction = UIAlertAction.init(title: "Cancelar", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        
+        //Playaction
+        let streamURLaction = UIAlertAction(title:"Play", style: .default) { (_) in
+            let urlTextField = alertController.textFields![0] as UITextField
+            let player = AVPlayer.init(url: URL.init(string: urlTextField.text!)!)
+            let playerViewController = CustomPlayerVC()
+            
+            playerViewController.player = player
+            self.present(playerViewController, animated: true, completion: {
+                playerViewController.player?.play()
+            })
+        }
+        streamURLaction.isEnabled = false
+
+        //adding textfield
+        alertController.addTextField { (textfield : UITextField) in
+            textfield.placeholder = "http//...."
+            NotificationCenter.default.addObserver(forName: NSNotification.Name.UITextFieldTextDidChange, object: textfield, queue: OperationQueue.main) { (notification) in
+                //habilitando o streaming action
+                streamURLaction.isEnabled = textfield.text != ""
+            }
+        }
+        
+        alertController.addAction(streamURLaction)
+        self.present(alertController, animated: true, completion: nil)
+
+        
+        
+        
+        
+    }
     @IBAction func Import(_ sender: AnyObject) {
 //        NSArray *types = [(NSString*)kUTTypeImage,(NSString*)kUTTypeSpreadsheet,(NSString*)kUTTypePresentation,(NSString*)kUTTypeDatabase,(NSString*)kUTTypeFolder,(NSString*)kUTTypeZipArchive,(NSString*)kUTTypeVideo];
 
@@ -61,9 +98,6 @@ class VideoSelectionTableViewController: UIViewController, UITableViewDelegate, 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: VideoTableViewCell = tableView.dequeueReusableCell(withIdentifier: "videoCell", for: indexPath) as! VideoTableViewCell
-        //deselect
-        tableView.deselectRow(at: indexPath, animated: true)
-
         //Thumbnail image
         let image = videoModel.getThumbnails()[indexPath.row]
         cell.videosThumbnail.image = image
@@ -97,7 +131,10 @@ class VideoSelectionTableViewController: UIViewController, UITableViewDelegate, 
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //deselect
+        tableView.deselectRow(at: indexPath, animated: true)
 
+        
         let avAsset = videoModel.getVideoAssets()[indexPath.row]
         let myplayerItem = AVPlayerItem.init(asset: avAsset)
         let player = AVPlayer.init(playerItem: myplayerItem)
