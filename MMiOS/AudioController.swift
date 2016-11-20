7fc396946fac9403b1da2ca7a7fb50780969b13d
Mac <mac@macs-imac.local>
2016-11-20 14:44:27 -0200

    New Method for returning musics

--- MMiOS/AudioController.swift
+++ MMiOS/AudioController.swift
@@ -16,11 +16,13 @@ class AudioController{
     let MusicPlayer = MPMusicPlayerController()
     
     private var playing: Bool
-    private var lastMusic : String
+    private var lastMusic : [String]
+    private var returns : Int
     
     init(){
         self.playing = MusicPlayer.playbackState == .playing
-        self.lastMusic = (MusicPlayer.nowPlayingItem?.title)!
+        self.lastMusic = [String]()
+        returns = 0
     }
 
     func getPlaying() -> Bool{
@@ -61,8 +63,6 @@ class AudioController{
         MusicPlayer.prepareToPlay()
         
         self.play()
-        
-        MusicPlayer.setQueue(with: model.getSongsQuery())
     }
     
     
@@ -79,13 +79,16 @@ class AudioController{
     }
     
     func skipMusic(){
-        lastMusic = (MusicPlayer.nowPlayingItem?.title)!
+        returns = 0
+        lastMusic.append( (MusicPlayer.nowPlayingItem?.title)!)
+        MusicPlayer.setQueue(with: model.getSongsQuery())
         MusicPlayer.skipToNextItem()
     }
     
     func returnMusic(){
         //MusicPlayer.skipToPreviousItem()
-        self.playWithQueue(musics: model.getSongsQuery(), musicsLabel: (MusicPlayer.nowPlayingItem?.title)!)
+        self.playWithQueue(musics: model.getSongsQuery(), musicsLabel: lastMusic[lastMusic.count - returns - 1])
+        returns = returns+1
     }
     
     func shuffleMode(){
